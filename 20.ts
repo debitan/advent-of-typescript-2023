40065281754fe87f2bf2785633a773564bfd3780
David Matthews <dwm503@gmail.com>
2023-12-20 15:29:17 +0100

    day 20

---
+++ 20.ts
@@ -0,0 +1,107 @@
+type Letters = {
+  A: ["█▀█ ", "█▀█ ", "▀ ▀ "];
+  B: ["█▀▄ ", "█▀▄ ", "▀▀  "];
+  C: ["█▀▀ ", "█ ░░", "▀▀▀ "];
+  E: ["█▀▀ ", "█▀▀ ", "▀▀▀ "];
+  H: ["█ █ ", "█▀█ ", "▀ ▀ "];
+  I: ["█ ", "█ ", "▀ "];
+  M: ["█▄░▄█ ", "█ ▀ █ ", "▀ ░░▀ "];
+  N: ["█▄░█ ", "█ ▀█ ", "▀ ░▀ "];
+  P: ["█▀█ ", "█▀▀ ", "▀ ░░"];
+  R: ["█▀█ ", "██▀ ", "▀ ▀ "];
+  S: ["█▀▀ ", "▀▀█ ", "▀▀▀ "];
+  T: ["▀█▀ ", "░█ ░", "░▀ ░"];
+  Y: ["█ █ ", "▀█▀ ", "░▀ ░"];
+  W: ["█ ░░█ ", "█▄▀▄█ ", "▀ ░ ▀ "];
+  " ": ["░", "░", "░"];
+  ":": ["#", "░", "#"];
+  "*": ["░", "#", "░"];
+};
+
+type NormaliseIndex<Index extends string> =
+  Uppercase<Index> extends keyof Letters ? Uppercase<Index> : "/n";
+
+type PrettifyText<
+  Input extends Array<unknown>,
+  $Acc0 extends string = "",
+  $Acc1 extends string = "",
+  $Acc2 extends string = ""
+> = Input extends [infer Head extends Array<string>, ...infer Tail]
+  ? PrettifyText<
+      Tail,
+      `${$Acc0}${Head[0]}`,
+      `${$Acc1}${Head[1]}`,
+      `${$Acc2}${Head[2]}`
+    >
+  : [$Acc0, $Acc1, $Acc2];
+
+type Build<
+  Input,
+  $Acc extends Array<unknown> = []
+> = Input extends `${infer Head}${infer Tail}`
+  ? NormaliseIndex<Head> extends keyof Letters
+    ? Build<Tail, [...$Acc, Letters[NormaliseIndex<Head>]]>
+    : never
+  : PrettifyText<$Acc>;
+
+type ToAsciiArt<
+  Input,
+  $Acc extends Array<unknown> = []
+> = Input extends `${infer Line}\n${infer Rest}`
+  ? ToAsciiArt<Rest, [...$Acc, ...Build<Line>]>
+  : [...$Acc, ...Build<Input>];
+
+// TESTS
+import { Equal, Expect } from "type-testing";
+
+type test_0_actual = ToAsciiArt<"   * : * Merry * : *   \n  Christmas  ">;
+type test_0_expected = [
+  "░░░░░#░░░█▄░▄█ █▀▀ █▀█ █▀█ █ █ ░░░#░░░░░",
+  "░░░#░░░#░█ ▀ █ █▀▀ ██▀ ██▀ ▀█▀ ░#░░░#░░░",
+  "░░░░░#░░░▀ ░░▀ ▀▀▀ ▀ ▀ ▀ ▀ ░▀ ░░░░#░░░░░",
+  "░░█▀▀ █ █ █▀█ █ █▀▀ ▀█▀ █▄░▄█ █▀█ █▀▀ ░░",
+  "░░█ ░░█▀█ ██▀ █ ▀▀█ ░█ ░█ ▀ █ █▀█ ▀▀█ ░░",
+  "░░▀▀▀ ▀ ▀ ▀ ▀ ▀ ▀▀▀ ░▀ ░▀ ░░▀ ▀ ▀ ▀▀▀ ░░"
+];
+type test_0 = Expect<Equal<test_0_actual, test_0_expected>>;
+
+type test_1_actual = ToAsciiArt<"  Happy new  \n  * : * : * Year * : * : *  ">;
+type test_1_expected = [
+  "░░█ █ █▀█ █▀█ █▀█ █ █ ░█▄░█ █▀▀ █ ░░█ ░░",
+  "░░█▀█ █▀█ █▀▀ █▀▀ ▀█▀ ░█ ▀█ █▀▀ █▄▀▄█ ░░",
+  "░░▀ ▀ ▀ ▀ ▀ ░░▀ ░░░▀ ░░▀ ░▀ ▀▀▀ ▀ ░ ▀ ░░",
+  "░░░░#░░░#░░░█ █ █▀▀ █▀█ █▀█ ░░░#░░░#░░░░",
+  "░░#░░░#░░░#░▀█▀ █▀▀ █▀█ ██▀ ░#░░░#░░░#░░",
+  "░░░░#░░░#░░░░▀ ░▀▀▀ ▀ ▀ ▀ ▀ ░░░#░░░#░░░░"
+];
+type test_1 = Expect<Equal<test_1_actual, test_1_expected>>;
+
+type test_2_actual =
+  ToAsciiArt<"  * : * : * : * : * : * \n  Trash  \n  * : * : * : * : * : * ">;
+type test_2_expected = [
+  "░░░░#░░░#░░░#░░░#░░░#░░░",
+  "░░#░░░#░░░#░░░#░░░#░░░#░",
+  "░░░░#░░░#░░░#░░░#░░░#░░░",
+  "░░▀█▀ █▀█ █▀█ █▀▀ █ █ ░░",
+  "░░░█ ░██▀ █▀█ ▀▀█ █▀█ ░░",
+  "░░░▀ ░▀ ▀ ▀ ▀ ▀▀▀ ▀ ▀ ░░",
+  "░░░░#░░░#░░░#░░░#░░░#░░░",
+  "░░#░░░#░░░#░░░#░░░#░░░#░",
+  "░░░░#░░░#░░░#░░░#░░░#░░░"
+];
+type test_2 = Expect<Equal<test_2_actual, test_2_expected>>;
+
+type test_3_actual =
+  ToAsciiArt<"  : * : * : * : * : * : * : \n  Ecyrbe  \n  : * : * : * : * : * : * : ">;
+type test_3_expected = [
+  "░░#░░░#░░░#░░░#░░░#░░░#░░░#░",
+  "░░░░#░░░#░░░#░░░#░░░#░░░#░░░",
+  "░░#░░░#░░░#░░░#░░░#░░░#░░░#░",
+  "░░█▀▀ █▀▀ █ █ █▀█ █▀▄ █▀▀ ░░",
+  "░░█▀▀ █ ░░▀█▀ ██▀ █▀▄ █▀▀ ░░",
+  "░░▀▀▀ ▀▀▀ ░▀ ░▀ ▀ ▀▀  ▀▀▀ ░░",
+  "░░#░░░#░░░#░░░#░░░#░░░#░░░#░",
+  "░░░░#░░░#░░░#░░░#░░░#░░░#░░░",
+  "░░#░░░#░░░#░░░#░░░#░░░#░░░#░"
+];
+type test_3 = Expect<Equal<test_3_actual, test_3_expected>>;
